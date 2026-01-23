4a5805504acdde377f2035afb42931561485f3ee
XZHHHHHH <xuzihaotm66@gmail.com>
2026-01-23 16:26:25 +0800

    Rename bot, Add greet, Add exit

---
+++ src/main/java/ZH9072Bot.java
@@ -0,0 +1,15 @@
+public class ZH9072Bot {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm ZH9072Bot");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
