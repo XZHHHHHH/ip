3c7d54fea9e2cfc5df20fb0154119211e8742c7d
XZHHHHHH <xuzihaotm66@gmail.com>
2026-01-26 18:58:21 +0800

    Add task

---
+++ src/main/java/Task.java
@@ -0,0 +1,13 @@
+public class Task {
+    private String content;
+
+    public Task(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return content;
+    }
+    
+}
