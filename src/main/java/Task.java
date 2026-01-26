13b6dda9d10d320f3222df5bbe12fcab6ff34a6b
XZHHHHHH <xuzihaotm66@gmail.com>
2026-01-26 20:05:40 +0800

    Add mark function

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,13 +1,31 @@
 public class Task {
     private String content;
+    private boolean isDone;
 
     public Task(String content) {
         this.content = content;
+        this.isDone = false;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void unMark() {
+        this.isDone = false;
+    }
+
+    public String iconStatus() {
+        if(isDone == true) {
+            return "[X]";
+        } else {
+            return "[ ]";
+        }
     }
 
     @Override
     public String toString() {
-        return content;
+        return iconStatus() + " " + content;
     }
     
 }
