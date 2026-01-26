5f822f4a91e78e413c4747d7ca0da4cabaf4c430
XZHHHHHH <xuzihaotm66@gmail.com>
2026-01-26 19:06:34 +0800

    Add list storage and display

--- src/main/java/ZH9072Bot.java
+++ src/main/java/ZH9072Bot.java
@@ -10,8 +10,11 @@ public class ZH9072Bot {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm ZH9072Bot");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
 
         Scanner sc = new Scanner(System.in);
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
 
         while(true) {
             String input;
@@ -22,11 +25,20 @@ public class ZH9072Bot {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for(int i = 0; i < taskCount; i++) {
+                    int number = i + 1;
+                    System.out.println(number + "." + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = new Task(input);
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + input);
+                System.out.println("____________________________________________________________");
             }
-        
-            System.out.println("____________________________________________________________");
-            System.out.println(input);
-            System.out.println("____________________________________________________________");
         }
 
         
