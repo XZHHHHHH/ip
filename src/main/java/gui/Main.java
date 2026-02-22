ac3cd8a0a06c9c353cd31d04a5c958cdcd4fb6e6
XZHHHHHH <xuzihaotm66@gmail.com>
2026-02-23 03:33:29 +0800

    Fix import ordering

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -1,5 +1,7 @@
 package gui;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -7,8 +9,6 @@ import javafx.scene.image.WritableImage;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import zhbot.Zhbot;
-
-import java.io.IOException;
 
 /**
  * A GUI for Zhbot using FXML.
